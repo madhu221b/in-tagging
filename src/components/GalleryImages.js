6aae198dfb293018c5acfd9fe325831c3e5984ab
madhu221b <madhurapawar10@gmail.com>
2021-03-22 15:35:19 +0530

    Added more features

--- src/components/GalleryImages.js
+++ src/components/GalleryImages.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { useState, useEffect } from 'react';
 import axios from "axios"
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
@@ -7,14 +6,121 @@ import Grid from '@material-ui/core/Grid';
 import { Modal, Button } from 'antd';
 import 'antd/dist/antd.css';
 import {Table} from 'antd';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Typography } from 'antd';
+import Chart from "react-apexcharts";
 
 const bottomOffset = 73
 const leftOffset = 20
+const antIcon = <LoadingOutlined style={{ fontSize: 24}} spin />;
+const { Title } = Typography;
 
-let bottom = 750*0.11770063638687134 - 70
-let left = 1000*0.24561920762062073 - 12
-let w = 1000*0.2204837203025818
-let h = 750*0.45420312881469727
+let series = [{
+              name: 'Barcode Count',
+              data: []
+            }]
+
+let options = {
+              chart: {
+                height: 250,
+                width: 400,
+                type: 'bar',
+              },
+              plotOptions: {
+                bar: {
+                  borderRadius: 10,
+                  dataLabels: {
+                    position: 'top', // top, center, bottom
+                  },
+                }
+              },
+              dataLabels: {
+                enabled: true,
+                formatter: function (val) {
+                  return val;
+                },
+                offsetY: -20,
+                style: {
+                  fontSize: '10px',
+                  colors: ["#304758"]
+                }
+              },
+
+              xaxis: {
+                categories: [],
+                position: 'bottom',
+                axisBorder: {
+                  show: false
+                },
+                axisTicks: {
+                  show: false
+                },
+                crosshairs: {
+                  fill: {
+                    type: 'gradient',
+                    gradient: {
+                      colorFrom: '#D8E3F0',
+                      colorTo: '#BED1E6',
+                      stops: [0, 100],
+                      opacityFrom: 0.4,
+                      opacityTo: 0.5,
+                    }
+                  }
+                },
+                tooltip: {
+                  enabled: true,
+                }
+              },
+              yaxis: {
+                axisBorder: {
+                  show: false
+                },
+                axisTicks: {
+                  show: false,
+                },
+                labels: {
+                  show: false,
+                  formatter: function (val) {
+                    return val;
+                  }
+                }
+
+              },
+              title: {
+                text: 'Barcode Count',
+                floating: true,
+                offsetY: 0,
+                align: 'top',
+                style: {
+                  color: '#444'
+                }
+              }
+            }
+
+let seriesPie =  [0, 0]
+let optionsPie =    {
+                               chart: {
+                                 width: 380,
+                                 type: 'pie',
+                               },
+                               labels: ['Box','Barcode'],
+                               responsive: [{
+                                 breakpoint: 480,
+                                 options: {
+                                   chart: {
+                                     width: 200
+                                   },
+                                   legend: {
+                                     position: 'bottom'
+                                   }
+                                 }
+                               }]
+                             }
+//let bottom = 750*0.11770063638687134 - 70
+//let left = 1000*0.24561920762062073 - 12
+//let w = 1000*0.2204837203025818
+//let h = 750*0.45420312881469727
 
 const columns = [
   {
@@ -29,7 +135,7 @@ const columns = [
   }
 ];
 
-const classes = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -40,36 +146,37 @@ const classes = makeStyles((theme) => ({
   },
 }));
 
-class GalleryImages extends React.Component {
+export default function GalleryImages(){
 
+   const classes = useStyles();
+   const [image, setImage] = useState(null);
+   const [imageArray, setImageArray] = useState(null)
+   const [loading, setLoading] = useState(true)
+   const [isModalVisible, setIsModalVisible] = useState(false)
+   const [showModalContent, setShowModalContent] = useState(null)
+   const [seriesBar, setSeriesBar] = useState(series)
+   const [optionsBar, setOptionsBar] = useState(options)
 
-  constructor(props){
-  super(props)
-  this.state = {image:null, imageArray : null,
-  loading: false,isModalVisible:false, showModalContent:null}
+ useEffect(() => {
+    axios.get('http://127.0.0.1:5000/')
+           .then((response) => {
+    //         let data = response.data.ImageBytes
+    //         this.setState({image:"data:image/png;base64," + data})
+             let imgArray = response.data
+             console.log(imgArray)
+             // this.setState({imageArray:imgArray,loading:false})
+             setImageArray(imgArray)
+             setLoading(false)
+             console.log(response.data);
+             console.log(response.status);
+             console.log(response.statusText);
+             console.log(response.headers);
+             console.log(response.config);
+           });
+  });
 
-  }
+  const onHideShow = (id, class_) => {
 
-  componentDidMount()
-  {
-     this.setState({loading:true}, () => {axios.get('http://127.0.0.1:5000/')
-       .then((response) => {
-//         let data = response.data.ImageBytes
-//         this.setState({image:"data:image/png;base64," + data})
-         let imgArray = response.data
-         console.log(imgArray)
-         this.setState({imageArray:imgArray,loading:false})
-         console.log(response.data);
-         console.log(response.status);
-         console.log(response.statusText);
-         console.log(response.headers);
-         console.log(response.config);
-       });
-       });
-  }
-
-  onHideShow = (id, class_) => {
-     console.log(id, class_)
      let x = document.getElementById(id)
      if(class_=="box"){
       if (x.style.border=== "none") {
@@ -87,7 +194,7 @@ class GalleryImages extends React.Component {
     }
   }
 
- openModal = (id, src, dim_array) => {
+  const openModal = (id, src, dim_array) => {
 
 
    let imgWidth = dim_array[0].imgSize[0] + "px"
@@ -99,7 +206,7 @@ class GalleryImages extends React.Component {
      dim_array.map((dim,i) =>
      {
       let div_id = id + i
-      console.log(dim, imgWidth, dim.rect , dim.rect[0], dim.rect[0][1])
+
       let img_width = dim.imgSize[0]
       let img_height = dim.imgSize[1]
       let bottom = img_height*dim.rect[0][1] + bottomOffset
@@ -113,7 +220,6 @@ class GalleryImages extends React.Component {
           color = "magenta"
       else if(class_=="barcode")
           color = "yellow"
-      console.log(bottom, left, w , h , arr)
       if(class_=="barcode")
       {
 
@@ -132,10 +238,10 @@ class GalleryImages extends React.Component {
       }
       }
     if(class_==="barcode")
-      arr.push(<div><div id={div_id} onClick={()=>this.onHideShow(div_id, class_)} style={{position:"absolute",bottom:bottom+"px", left:left+"px",
+      arr.push(<div><div id={div_id} onClick={()=>onHideShow(div_id, class_)} style={{position:"absolute",bottom:bottom+"px", left:left+"px",
        width:w+"px",height:h+"px",zIndex: 1,border:"2px solid "+ color}}/>{barcode_div}</div>)
        else
-       arr.push(<div><div id={div_id} onClick={()=>this.onHideShow(div_id, class_)} style={{position:"absolute",bottom:bottom+"px", left:left+"px",
+       arr.push(<div><div id={div_id} onClick={()=>onHideShow(div_id, class_)} style={{position:"absolute",bottom:bottom+"px", left:left+"px",
               width:w+"px",height:h+"px",border:"2px solid "+ color}}/></div>)
 
      })
@@ -150,18 +256,11 @@ class GalleryImages extends React.Component {
       "count":counter[item]
       }
       counter_array.push(row)
+
    }
 
 
-//    for (let item in counter) {
-//      console.log('key:' + item + ' value:' + counter[item]);
-//      if(item != "NA"){
-//      rows.push(<tr>
-//       <td>{item}</td>
-//       <td>{counter[item]}</td>
-//      </tr>)
-//    }
-//    }
+
     let table = <Table dataSource={counter_array} columns={columns} />
    let modalContent = (
    <table> <tr>
@@ -176,47 +275,39 @@ class GalleryImages extends React.Component {
      </td>
      </tr></table>)
 
-   this.setState({isModalVisible:true,showModalContent:modalContent})
+   // this.setState({isModalVisible:true,showModalContent:modalContent})
+   setIsModalVisible(true)
+   setShowModalContent(modalContent)
   };
 
-    handleOk = () => {
-       this.setState({isModalVisible:false})
+    const handleOk = () => {
+      // this.setState({isModalVisible:false})
+      setIsModalVisible(false)
    };
 
-    handleCancel = () => {
-      this.setState({isModalVisible:false})
+    const handleCancel = () => {
+      // this.setState({isModalVisible:false})
+      setIsModalVisible(false)
    };
 
-  onCreateGallery = () =>
+  const onCreateGallery = () =>
   {
      let arr = []
-  if(this.state.imageArray){
-   this.state.imageArray.map((image,i) => {
+  if(imageArray){
+   imageArray.map((image,i) => {
    let img_src = "data:image/png;base64,"+image.ImageBytes
    let img_id = image.id
    let dim_array = image.data
+
     arr.push(
     <Grid item xs={2}>
-      <Paper className={classes.paper} onClick={()=> this.openModal(img_id, img_src, dim_array)}><img id={img_id} src={img_src}
-      style={{width:"60px",height:"40px"}}/></Paper>
+      <Paper className={classes.paper} onClick={()=> openModal(img_id, img_src, dim_array)}><img id={img_id} src={img_src}
+      style={{width:"100px",height:"80px"}}/></Paper>
     </Grid>
     )
    })
    }
-//     this.state.imageArray.map(image =>
-//     {
-//      let boxes = []
-//      let dim_array = image.data
-//      {console.log(dim_array)}
-//      arr.push(
-//      <Grid item xs={3}>
-//      <Paper className={classes.paper}>Yes</Paper>
-//      </Grid>
-//      );
-//     })
-//     arr = <Grid item xs={3}>
-//                         <Paper className={classes.paper}>Yes</Paper>
-//                       </Grid>;
+
      return (<div className={classes.root}>
           <Grid container
                   direction="row"
@@ -229,25 +320,29 @@ class GalleryImages extends React.Component {
 
   }
 
-  generateView = () => {
+  const generateView = () => {
 
 
     let counter = {}
-    if(this.state.imageArray){
-  this.state.imageArray.map(image => {
+    //For pie chart
+    seriesPie = [0, 0]
+    if(imageArray){
+  imageArray.map(image => {
 
      image.data.map(dim => {
-          if(dim.className == "barcode"){
+          if(dim.className === "barcode"){
          let code = dim.code
-         console.log("Code", code)
-           if(code !== "NA"){
-            console.log("Code in if clause", code)
-               if(counter.hasOwnProperty(code))
+          if(code !== "NA"){
+            if(counter.hasOwnProperty(code))
                counter[code] += 1
                else
                counter[code] = 1
                }
+               seriesPie[1] += 1
         }
+        else if(dim.className === "box" )
+          seriesPie[0] += 1
+
 
      })
 
@@ -256,6 +351,9 @@ class GalleryImages extends React.Component {
   })
  }
   let counter_array = []
+
+       series[0].data = []
+       options.xaxis.categories = []
         for(let item in counter){
         let row =
         {
@@ -263,11 +361,26 @@ class GalleryImages extends React.Component {
         "count":counter[item]
         }
         counter_array.push(row)
+        // For summarisation graphs
+        series[0].data.push(counter[item])
+        options.xaxis.categories.push(item)
      }
+
   return <Table dataSource={counter_array} columns={columns} />
   }
 
-  render() {
+  const generateGraphs = () => {
+
+
+  return (
+    <div>
+      <Chart options={options} series={series} height={400} width={900} type="bar" />
+      <Chart options={optionsPie} series={seriesPie} width={380} type="pie" />
+    </div>
+  );
+
+
+  }
 
     return(
 //    <div>
@@ -276,17 +389,23 @@ class GalleryImages extends React.Component {
 //    width:w+"px",height:h+"px",border:"3px solid #FF00FF"}}/>
 //    </div>
       <div>
-     {this.state.loading? <div>"Data is loading"</div>:
+     {loading?
+      <div style={{marginLeft: "50%", marginTop: "20%"}}>
+     <Spin indicator={antIcon} />
+     </div>:
      <div>
-     {this.onCreateGallery()}
-     {this.generateView()}
+     <Title level={2}>Gather.ai Images Captured by Drone</Title>
+     {onCreateGallery()}
+     <Title level={2}>Warehouse-level Item Summary View</Title>
+     {generateView()}
+     <Title level={2}>Summarization Graphs</Title>
+     {generateGraphs()}
      </div>}
-       <Modal width="800" title="Basic Modal" visible={this.state.isModalVisible} onOk={this.handleOk} onCancel={this.handleCancel}>
-             {this.state.showModalContent}
+       <Modal width="800" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+             {showModalContent}
            </Modal>
      </div>
 );
-  }
+
 }
 
-export default GalleryImages;
